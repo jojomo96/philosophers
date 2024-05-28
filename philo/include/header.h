6026a07b29e8acedd5925adfaf483d33341dbcb7
jojomo96 <jmoritz@student.42heilbronn.de>
2024-05-28 22:54:22 +0200

    input parsing

--- philo/include/header.h
+++ philo/include/header.h
@@ -6,13 +6,40 @@
 /*   By: jojomo96 <jojomo96@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/28 11:27:26 by jojomo96          #+#    #+#             */
-/*   Updated: 2024/05/28 12:39:04 by jojomo96         ###   ########.fr       */
+/*   Updated: 2024/05/28 22:48:49 by jojomo96         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef HEADER_H
 # define HEADER_H
 
+# include <pthread.h>
+# include <stdio.h>
+# include <stdlib.h>
 
+typedef struct s_philo
+{
+    int				id;
+    int				left_fork;
+    int				right_fork;
+    int				meals;
+    int				last_meal;
+    pthread_t		thread;
+}					t_philo;
+
+typedef struct s_data
+{
+    int				philo_count;
+    int				time_to_die;
+    int				time_to_eat;
+    int				time_to_sleep;
+    int				meal_count;
+    int				dead;
+    int				full;
+    int				start;
+    t_philo			*philos;
+    pthread_mutex_t	*forks;
+    pthread_mutex_t	print;
+}					t_data;
 
 #endif
---
+++ philo/src/input_parsing.c
@@ -0,0 +1,87 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   input_parsing.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jojomo96 <jojomo96@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/28 20:01:04 by jojomo96          #+#    #+#             */
+/*   Updated: 2024/05/28 22:47:56 by jojomo96         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "header.h"
+
+int	ft_atoi(const char *str)
+{
+    int		i;
+    int		sign;
+    long	nbr;
+
+    i = 0;
+    sign = 1;
+    nbr = 0;
+    while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n'
+        || str[i] == '\v' || str[i] == '\f' || str[i] == '\r')
+        i++;
+    if (str[i] == '-' || str[i] == '+')
+    {
+        if (str[i] == '-')
+            sign = -1;
+        i++;
+    }
+    while (str[i] >= '0' && str[i] <= '9')
+    {
+        nbr = nbr * 10 + (str[i] - '0');
+        i++;
+    }
+    return (nbr * sign);
+}
+
+int    ft_parse_args(t_data *data, int argc, char **argv)
+{
+    if (argc < 5 || argc > 6)
+        return (1);
+    data->philo_count = ft_atoi(argv[1]);
+    data->time_to_die = ft_atoi(argv[2]);
+    data->time_to_eat = ft_atoi(argv[3]);
+    data->time_to_sleep = ft_atoi(argv[4]);
+    if (argc == 6)
+        data->meal_count = ft_atoi(argv[5]);
+    else
+        data->meal_count = -1;
+    // if (data->philo_count < 2 || data->time_to_die < 60
+    //     || data->time_to_eat < 60 || data->time_to_sleep < 60
+    //     || (argc == 6 && data->meal_count < 1))
+    //     return (1);
+    return (0);
+}
+
+int    ft_init_data(t_data *data, int argc, char **argv)
+{
+    if (ft_parse_args(data, argc, argv))
+        return (1);
+    data->dead = 0;
+    data->full = 0;
+    data->start = 0;
+    data->philos = malloc(sizeof(t_philo) * data->philo_count);
+    if (!data->philos)
+        return (1);
+    data->forks = malloc(sizeof(pthread_mutex_t) * data->philo_count);
+    if (!data->forks)
+    {
+        free(data->philos);
+        return (1);
+    }
+    pthread_mutex_init(&data->print, NULL);
+    for (int i = 0; i < data->philo_count; i++)
+    {
+        data->philos[i].id = i + 1;
+        data->philos[i].left_fork = i;
+        data->philos[i].right_fork = (i + 1) % data->philo_count;
+        data->philos[i].meals = 0;
+        data->philos[i].last_meal = 0;
+        pthread_mutex_init(&data->forks[i], NULL);
+    }
+    return (0);
+}
